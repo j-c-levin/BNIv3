d6f279590d4f01ae34320be93953ffcef1e61dc3
Joshua Levin <joshua.c.levin@gmail.com>
2017-09-03 19:22:42 +0100

    camera moves between points

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public delegate void reachedDestination();
+    public float movementSpeed;
+    public reachedDestination destinationDelegate;
+    private Transform target;
+	private Transform source;
+    public void FixedUpdate()
+    {
+        if (target != null)
+        { 
+            Vector3 newPosition = Vector3.MoveTowards(source.position, target.position, movementSpeed);
+            // Keep the camera at the same height and move only in a 2d plane
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+            if (CameraAtTarget() && DelegateAssigned())
+            {
+				target = null;
+                destinationDelegate();
+            }
+        }
+    }
+
+    public void SetTarget(GameObject target)
+    {
+		this.source = transform;
+        this.target = target.transform;
+    }
+
+    private bool CameraAtTarget()
+    {
+        // We only need to check x and y in 2d
+        bool xMatches = ApproximateLocation(transform.position.x, target.position.x);
+        bool yMatches = ApproximateLocation(transform.position.y, target.position.y);
+        return xMatches && yMatches;
+    }
+
+    private bool ApproximateLocation(float source, float destination)
+    {
+        return Mathf.Abs(destination - source) <= 0.2;
+    }
+
+    private bool DelegateAssigned()
+    {
+        return destinationDelegate != null;
+    }
+}
---
+++ Assets/Scripts/test_CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_CameraController : MonoBehaviour {
+	public GameObject target1;
+	public GameObject target2;
+	public CameraMovement gameCamera;
+
+	private GameObject currentTarget;
+	// Use this for initialization
+	void Start () {
+		gameCamera.destinationDelegate = CameraDelegate; 
+		gameCamera.SetTarget(target1);	
+		currentTarget = target1;
+	}
+
+	void CameraDelegate() {
+		GameObject target = (currentTarget == target1) ? target2 : target1;
+		currentTarget = target;
+		gameCamera.SetTarget(target);
+	}
+}
