e136b0ecddec7093126cb9ce375731f3217c051d
Joshua Levin <joshua.c.levin@gmail.com>
2017-09-13 23:45:01 +0100

    remove debug logs

--- Assets/Scripts/RaceStartController.cs
+++ Assets/Scripts/RaceStartController.cs
@@ -31,7 +31,6 @@ public class RaceStartController : MonoBehaviour
             Debug.LogError("Spawn controller not attached");
         }
         movementController.SetRaceRunning(false);
-        Debug.Log("Race NOT running");
         GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMovement>().enabled = false;
         playersReady = new Dictionary<int, float>();
         isPlayerReady = new Dictionary<int, bool>();
@@ -54,7 +53,6 @@ public class RaceStartController : MonoBehaviour
         }
         if (allReadyForRace && spawnController.players.Count > 0)
         {
-            Debug.Log("all ready for race");
             StartRace();
         }
     }
@@ -62,7 +60,6 @@ public class RaceStartController : MonoBehaviour
     private void StartRace()
     {
         movementController.SetRaceRunning(true);
-        Debug.Log("Race running");
         foreach (KeyValuePair<int, GameObject> entry in spawnController.players)
         {
             entry.Value.GetComponentInChildren<PlayerMovement>().JumpUp();
@@ -117,7 +114,6 @@ public class RaceStartController : MonoBehaviour
             playersReady[entry.Key] += Time.deltaTime;
         }
         playersReady[entry.Key] = Mathf.Clamp(playersReady[entry.Key], 0, readyUpTimerDuration);
-        Debug.Log(playersReady[entry.Key]);
     }
 
 
