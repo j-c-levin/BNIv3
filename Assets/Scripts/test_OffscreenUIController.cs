34afc08ed9bc0151896a4f38b7af88b375ef8b3a
Joshua Levin <joshua.c.levin@gmail.com>
2017-09-13 16:44:49 +0100

    get the player offscreen indicator working in testing.  time to move it over into the main race

--- Assets/Scripts/test_OffscreenUIController.cs
+++ Assets/Scripts/test_OffscreenUIController.cs
@@ -9,6 +9,8 @@ public class test_OffscreenUIController : MonoBehaviour, IOffscreenUIController
     public Image offscreenIndicator;
     public float xClamp;
     public float yClamp;
+    private float playerXEdge = 8f;
+    private float playerYEdge = 4f;
     private test_SpawnController spawnController;
 
     public void Start()
@@ -18,11 +20,37 @@ public class test_OffscreenUIController : MonoBehaviour, IOffscreenUIController
 
     public void Update()
     {
-        offscreenIndicator.rectTransform.localPosition = cam.WorldToViewportPoint(spawnController.playerMovement.transform.position);
+        float percentageX = (cam.transform.position.x - spawnController.playerMovement.transform.position.x) / playerXEdge;
+        float percentageY = (cam.transform.position.y - spawnController.playerMovement.transform.position.y) / playerYEdge;
+        percentageX = Mathf.Clamp(percentageX, -1, 1);
+        percentageY = Mathf.Clamp(percentageY, -1, 1);
+        if (percentageX == 1)
+        {
+            offscreenIndicator.transform.rotation = Quaternion.Euler(0, 0, 270);
+        }
+        else if (percentageX == -1)
+        {
+            offscreenIndicator.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        if (percentageY == 1)
+        {
+            offscreenIndicator.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (percentageY == -1)
+        {
+            offscreenIndicator.transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        percentageX *= xClamp * -1;
+        percentageY *= yClamp * -1;
+        Vector2 newIndicatorPosition = new Vector2(percentageX, percentageY);
+        offscreenIndicator.transform.localPosition = newIndicatorPosition;
     }
 
     public void PlayerOffscreen(int playerId, bool isOffscreen)
     {
-        // offscreenIndicator.enabled = isOffscreen;
+        if (offscreenIndicator)
+        {
+            offscreenIndicator.enabled = isOffscreen;
+        }
     }
 }
