9527c6af743b4a528449898360ee471a15ed984e
Joshua Levin <joshua.c.levin@gmail.com>
2017-09-19 13:42:01 +0100

    nice assets

---
+++ Assets/Scripts/FitColliderToObstacle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitColliderToObstacle : MonoBehaviour
+{
+    void Start()
+    {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		GetComponent<BoxCollider2D>().size = sprite.size;
+    }
+}
