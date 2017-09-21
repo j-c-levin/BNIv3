528888a040a90f38fc5acb5cc1493bb3395d560f
Joshua Levin <joshua.c.levin@gmail.com>
2017-09-21 14:25:36 +0100

    work on environment hazards

---
+++ Assets/Scripts/Hazards/castleDropPlatform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class castleDropPlatform : MonoBehaviour
+{
+    public delegate void EndOfHazardDelegate();
+
+    public EndOfHazardDelegate endOfHazardDelegate;
+    public float dropSpeed;
+    private bool drop = false;
+    private Rigidbody2D platform;
+
+    public void StartDrop()
+    {
+        platform = GetComponent<Rigidbody2D>();
+        drop = true;
+    }
+
+    public void Update()
+    {
+        if (drop == false)
+        {
+            return;
+        }
+        Vector2 newPosition = new Vector2(transform.position.x, transform.position.y - (dropSpeed * Time.deltaTime));
+        platform.MovePosition(newPosition);
+    }
+
+    public void OnBecameInvisible()
+    {
+        if (endOfHazardDelegate == null)
+        {
+            Debug.LogError("No end of hazard delegate");
+        }
+        endOfHazardDelegate();
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/test_EnvironmentController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_EnvironmentController : MonoBehaviour
+{
+    public EnvironmentController.environmentHazard hazard;
+    public float timeBetweenHazard;
+    public GameObject castleDropPlatform;
+    private Transform player;
+    private Transform mainCamera;
+
+    public void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        StartCoroutine("SpawnHazard");
+    }
+
+    private IEnumerator SpawnHazard()
+    {
+        yield return new WaitForSeconds(timeBetweenHazard);
+        switch (hazard)
+        {
+            case EnvironmentController.environmentHazard.FallingBlocks:
+                CastleDropPlatform();
+                break;
+        }
+    }
+
+    private void CastleDropPlatform()
+    {
+        Vector3 spawnPosition = player.position;
+        spawnPosition.y = 6f;
+        spawnPosition.z = 1f;
+        GameObject platform = Instantiate(castleDropPlatform, Vector2.zero, castleDropPlatform.transform.rotation);
+        platform.transform.SetParent(mainCamera.transform);
+        platform.transform.localPosition = spawnPosition;
+        platform.GetComponent<castleDropPlatform>().endOfHazardDelegate = EndOfHazard;
+    }
+
+    private void EndOfHazard()
+    {
+        StartCoroutine("SpawnHazard");
+    }
+}
