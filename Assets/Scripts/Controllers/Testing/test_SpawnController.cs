491234fe9eadeb1fde0ad3b4e3d0f6eeb6efd1cd
Joshua Levin <joshua.c.levin@gmail.com>
2017-09-28 00:11:14 +0100

    test spawn controller now has a respawn time...and the ugliest, most inefficient update loop i've written in years

--- Assets/Scripts/Controllers/Testing/test_SpawnController.cs
+++ Assets/Scripts/Controllers/Testing/test_SpawnController.cs
@@ -8,10 +8,32 @@ public class test_SpawnController : MonoBehaviour, ISpawnController
     public GameObject player;
     public PlayerMovement playerMovement;
     public PlayerStyle.Character character;
+    public float respawnDuration;
+    private float respawnTime = 0;
+    private bool playerHasSpawned = true;
 
     public void Start()
     {
         player.GetComponent<PlayerStyle>().SetCharacterStyle(character);
+    }
+
+    public void Update()
+    {
+        if (Time.time - respawnDuration < respawnTime)
+        {
+            GetComponent<test_MovementController>().enabled = false;
+            RespawnTime();
+        }
+        else if (playerHasSpawned == false)
+        {
+            playerHasSpawned = true;
+            GetComponent<test_MovementController>().enabled = true;
+            foreach (SpriteRenderer spriteRenderer in player.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.color = new Color(1, 1, 1, 1);
+                spriteRenderer.GetComponent<Collider2D>().enabled = true;
+            }
+        }
     }
 
     public void SpawnPlayer(GameObject player)
@@ -20,5 +42,23 @@ public class test_SpawnController : MonoBehaviour, ISpawnController
         this.player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
         this.player.GetComponent<PlayerStyle>().SetCharacterStyle(character);
         playerMovement = this.player.GetComponentInChildren<PlayerMovement>();
+        respawnTime = Time.time;
+        playerHasSpawned = false;
+        foreach (SpriteRenderer spriteRenderer in this.player.GetComponentsInChildren<SpriteRenderer>())
+        {
+            spriteRenderer.color = new Color(1, 1, 1, 0.5f);
+            spriteRenderer.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+    private void RespawnTime()
+    {
+        test_RaceStartController raceStartController = GetComponent<test_RaceStartController>();
+        float distanceFromStartHeight = raceStartController.raceStartHeight - player.GetComponentInChildren<PlayerMovement>().transform.position.y;
+        float upwardForce = raceStartController.neutralForce * ((distanceFromStartHeight > 0) ? 1 : 0f);
+        player.GetComponentInChildren<PlayerMovement>().gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, upwardForce);
+        float rotationFromNeutral = (player.GetComponentInChildren<PlayerMovement>().transform.rotation.eulerAngles.z <= 180) ? -player.GetComponentInChildren<PlayerMovement>().transform.rotation.eulerAngles.z : 360 - player.GetComponentInChildren<PlayerMovement>().transform.rotation.eulerAngles.z;
+        float torqueForce = raceStartController.rotationForce * rotationFromNeutral;
+        player.GetComponentInChildren<PlayerMovement>().gameObject.GetComponent<Rigidbody2D>().AddTorque(torqueForce, ForceMode2D.Force);
     }
 }
